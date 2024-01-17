6d3afb57fca261b6658b8eb1dabef9ef529927a3
Fer <fernandomartinezortiz003@gmail.com>
2024-01-17 11:12:45 +0100

    Ejercicio de buscador hecho con componentes

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -3,11 +3,35 @@ import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
+function Buscador(props) {
+
+  return (
+    <div>
+      <p><label>Buscador:</label></p>
+      <input type='text' id='buscador' placeholder='introduce un nombre' onChange={props.buscarpueblo} />
+      <div>
+        <p>Resultados de la búsqueda:</p>
+        <ul>
+          {props.resultado.map((pueblo, index) => (
+            <li key={index}>{pueblo}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+
+  );
+
+} 
+
+
+
+
 function App() {
-  const [pueblos, setPueblos] = useState(["Estepona", "Estepa", "Escañela", "Leon", "Alcorcon"]);
+  const pueblos = ["Estepona", "Estepa", "Escañela", "Leon", "Alcorcon"];
   const [resultadosBusqueda, setResultadosBusqueda] = useState([]);
 
-  const buscador = (event) => {
+  const handleChange = (event) => {
     const datosBuscador = event.target.value.toLowerCase();
     //startsWith que empieza por el dato
     //endWith acaba por el dato
@@ -18,22 +42,12 @@ function App() {
 
   return (
     <div className="App">
-      <p>
-        <label>Buscador:</label>
-      </p>
-      <input type='text' id='buscador' placeholder='introduce un nombre' onChange={buscador}/>
-      <div>
-        <p>Resultados de la búsqueda:</p>
-        <ul>
-          {resultadosBusqueda.map((pueblo, index) => (
-            <li key={index}>{pueblo}</li>
-          ))}
-        </ul>
-      </div>
+      <Buscador
+        buscarpueblo={handleChange}
+        resultado={resultadosBusqueda}
+      />
     </div>
   );
 }
 
 export default App;
-
-
