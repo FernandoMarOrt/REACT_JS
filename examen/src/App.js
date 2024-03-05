27745728a5eb03521f65c0ad1fb34318947f7059
fer <fernandomartinezortiz003@gmail.com>
2024-03-05 12:40:43 +0100

    PISOS

--- examen/src/App.js
+++ examen/src/App.js
@@ -1,442 +1,214 @@
-import React, { Component, useState } from "react";
-import { Button, Input, FormGroup, Label, Col, Table, ButtonGroup} from 'reactstrap';
-import "bootstrap/dist/css/bootstrap.min.css";
-
-
-const Saldo=(props)=>{
-//recuerda que puedes pasar const {}
-  //GESTIÓN DE SALDO (SUMAR Y GASTAR)
-  const [telefono,setTelefono]= useState('');
-  const [saldo,setSaldo]=useState('');
-  const [mensaje,setMensaje]=useState('');
-
- const handleChange = (event) => {
-  let target=event.target;
-  if(target.name=="telefono"){
-    setTelefono(target.value);
-  }
-  if(target.name=="saldo"){
-    setSaldo(target.value);
-  }
- }
-
- const enviar=()=>{
-  //VIKI POR FUNCIONALIDAD PARA QUE SEAN NUMEROS
- if( telefono !="" && saldo !="" ){
-
-  if(telefono>0){
-    console.log(telefono)
-    console.log(saldo)
-    props.actualizar(telefono,saldo);
-  }else{
-  setMensaje("Debes insertar números ")
-  }
-  
-
-  
- }else{
-  setMensaje("Debes rellenar todos los campos para actualizar saldo")
- }
-  
- }
-
-
- return (
-   <div>
-     <h3>{props.titulo}</h3>
-     <FormGroup row>
-       <Label sm={1} > Teléfono: </Label>
-       <Col sm={2}>
-         <Input
-           id="telefono"
-           name="telefono"
-           type="Text" onChange={handleChange} />
-       </Col>
-       <Label sm={1} > Saldo: </Label>
-       <Col sm={2}>
-         <Input
-           id="saldo"
-           name="saldo"
-           type="Number" onChange={handleChange} />
-       </Col>
-     </FormGroup>
-
-
-     <Button color="primary" onClick={()=>enviar(telefono,saldo)}>ACTUALIZAR</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-   </div>
-
-
- ); 
-}
-
-
-const Altas = (props) => {
-// ALTAS DE USUARIOS
-const [nombre,setNombre]= useState('');
-const [telefono,setTelefono]= useState('');
-const [saldo,setSaldo]=useState('');
-
-const [mensaje,setMensaje]=useState('');
-
- const handleChange = (event) => {
-  let target=event.target;
-
-  if(target.name=="nombre"){
-    setNombre(target.value);
-  }
-  if(target.name=="telefono"){
-    setTelefono(target.value);
-  }
-  if(target.name=="saldo"){
-    setSaldo(target.value);
-  }
- }
-
- const enviar=()=>{
-  //VIKI POR FUNCIONALIDAD PARA QUE SEAN NUMEROS
- if(nombre !="" && telefono!="" && saldo !="" ){
-  
-  props.alta(nombre,telefono,saldo);
- }else{
-  setMensaje("Debes rellenar todos los campos para insertar")
- }
-  
- }
-
-
- return (
-   <div>
-     <h3>Alta de usuario</h3>
-     <FormGroup row>
-       <Label sm={1} > Nombre: </Label>
-       <Col sm={3}>
-         <Input
-           id="nombre"
-           name="nombre"
-           type="Text" onChange={handleChange} />
-       </Col>
-       <Label sm={1} > Teléfono: </Label>
-       <Col sm={2}>
-         <Input
-           id="telefono"
-           name="telefono"
-           type="Text" onChange={handleChange} />
-       </Col>
-       <Label sm={1} > Saldo: </Label>
-       <Col sm={2}>
-         <Input
-           id="saldo"
-           name="saldo"
-           type="Number" onChange={handleChange} />
-       </Col>
-     </FormGroup>
-
-
-     <Button color="primary" onClick={()=>enviar()}>ALTA</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <p>{mensaje}</p>
-   </div>
-
-
- );
-}
-
-
-const Mostrar = (props) => {
- // ESTE COMPONENTE MUESTRA LA TABLA
- //recibo datos y borrar
-  let lista=props.datos;
- // console.log(lista)
-  const {datos}=props;
- // console.log(datos)
- const [usuario,setUsuario]=useState('');
-
+import {
+  Modal, ModalHeader, ModalBody, ModalFooter, Button, Toast,
+  ToastBody, ToastHeader
+} from 'reactstrap';
+import React, { useState } from 'react';
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+/* ENUNCIADO DEL EJERCICIO.
+Se pide que realices el programa que visualizas en el vídeo adjunto.
+Se considerará incorrecto un programa que acumule la funcionalidad en solo
+un componente o cambie la estructura de clases.
+Cosas que se valorarán:
+Legibilidad del código.
+Longitud del código.
+No se valorará el aspecto estético de la web.
+El programa realiza la funcionalidad requerida.
+Que no haya errores en la consola.
+La eficiencia.
+La modularidad (alta cohesión y bajo acoplamiento).
+Que el código sea limpio, flexible, reutilizable y mantenible.
+Sigue los principios SOLID.
+Otros criterios.
+A continuación se describen los componentes que tienes que implementar
+para completar este ejercicio:
+-----------COMPONENTE PREGUNTAS-----------
+Es el encargado de mostrar la pregunta al usuario
+-----------COMPONENTE VENTANAMODAL-----------
+Es el encargado de mostrar el resultado del test y preguntar si lo quiere
+hacer de nuevo
+-----------PREGUNTAS------------
+Las preguntas se muestran de manera aleatoria. Para ello te proporciono la
+función aleatorio que siempre hemos utilizado.
+Importante const PREGUNTAS:
  
- return (
-   <>
-     <Table striped bordered hover size="sm">
-       <thead>
-         <tr>
-           <th></th>
-           <th>Teléfono</th>
-           <th>Nombre</th>
-           <th>Saldo</th>
-         </tr>
-       </thead>
-       <tbody>
-       <tr>
-     <td>Boton</td>
-     <td>telefono</td>
-     <td>nombre</td>
-     <td>saldo</td>
-   </tr>
+EN LAS PREGUNTAS SI RESPUESTA ES 1 Y EL USUARIO ESCOJE SI -> ES
+NOMOFÓBICO (No nomofóbico en caso contrario)
+Y SI EL USUARIO ELIGE NO Y RESPUESTA ES 0 -> EL USUARIO ES NOMOFÓBICO (No
+nomofóbico en caso contrario)
+-----------RESPUESTAS-----------
+DEPENDIENDO DE LAS RESPUESTAS:
+Al final de las preguntas y atendiendo las respuestas...
+Si el porcentaje de las respuestas es < 50 mostrará En principio no
+tienes nada de que preocuparte"
+Si el porcentaje de las respuestas es entre 50-69 mostrará "Empiezas a
+tener signos de dependencia del móvil. Puedes utilizar técnicas como
+apagar el móvil cuando no lo necesitas, cuando duermes, etc."
+Si el porcentaje de las respuestas es entre 70-79 mostrará "Tienes una
+gran dependencia del móvil. Deberías seguir un plan de «desintoxicación»
+del móvil como por ejemplo dejar el móvil en casa cuando vas a comprar,
+apagarlo durante la noche, apagarlo durante horas de clase o trabajo,
+etc."
+Si el porcentaje de las respuestas es >= 80 mostrará "Tus síntomas de
+dependencia son muy preocupantes. Además de todas las técnicas de los
+apartados anteriores deberías plantearte un plan de desintoxicación del
+móvil que consista en estar una o dos semanas sin utilizarlo. Si ves que
+no puedes hacerlo por ti mismo, pide ayuda a un profesional."
+*/
+const PREGUNTAS = [
+  {
+    "pregunta": "Cuando mandas un mensaje por whatsapp esperas siempre al doble check.Si no aparece vuelves a abrir el terminal para revisarlo alrato.","respuesta":1},
+  {"pregunta": "Antes de acostarte siempre miras el móvil a ver si tienemensajes o notificaciones.","respuesta":1},
+  {"pregunta": "Te despiertas antes de tiempo para jugar, mandar mensajes,actualizar perfiles, ...con el teléfono móvil.","respuesta":1},
+  {"pregunta": "Si sales de casa sin el móvil volverías a cogerlo aunque llegues tarde a tu cita.","respuesta":1},
+  { "pregunta": "No tienes miedo a quedarte sin batería.", "respuesta": 0 },
+  {"pregunta": "Cuando tienes la batería baja desactivas ciertas aplicaciones u opciones del teléfono como la WiFi, bluetooth para no quedarte sin batería.","respuesta":1},
   
-    {datos.map((x,index)=>{
-   return ( <tr>
-    <td><Button name="borrar" key={index} value={index} 
-    onClick={()=>props.borrar(x)}>Borrar {index}</Button></td>
-      <td>{x.telefono}</td>
-      <td>{x.nombre}</td>
-      <td>{x.saldo}</td>
-      </tr>
-      );
-    })}
-   
-       </tbody>
-     </Table>
+  {"pregunta": "Tienes ansiedad cuando tienes llamadas perdidas. Llamas a los números y te preocupas si no responden.","respuesta":1},
+  {"pregunta": "Miras la cobertura cuando estas en algún sitio con los amigos, esperando, etc.","respuesta":1},
+  {"pregunta": "Sueles hacer alguna otra cosa mientras que miras al móvilcomo comer, lavarte los dientes, etc.","respuesta":1},
+  {"pregunta": "No sueles mirar la cobertura del móvil con frecuencia","respuesta":0},
+  { "pregunta": "Vas al baño siempre con el móvil.", "respuesta": 1 },
+  { "pregunta": "A veces sales a la calle sin el móvil.", "respuesta": 0 }
+];
 
+const Preguntas = (props) => {
+  return (
+    <div className="p-3 my-2 rounded bg-docs-transparent-grid">
+      <Toast>
+        <ToastHeader>
+          {props.cabecera}
+        </ToastHeader>
+        <ToastBody>
+          {props.texto}
+        </ToastBody>
+        <div class="container">
+          <div class="row justify-content-center">
+            <Button outline color="secondary"
+              onClick={() => props.si()}>Si</Button>&nbsp;
+            <Button outline color="secondary"
 
-   </>
- );
-};
+              onClick={() => props.no()}>No</Button>
 
+          </div>
+        </div>
+      </Toast>
+    </div>
+  );
+}
+const VentanaModal = (props) => {
 
-
-
-
-
+  const {
+    className
+  } = props;
+  const [modal, setModal] = useState(true);
+  const toggle = () => setModal(!modal);
+  const seguir = () => { props.seguir(); setModal(!modal); };
+  return (
+    <div>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>{props.titulo}</ModalHeader>
+        <ModalBody>
+          {props.texto}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary"
+            onClick={seguir}>{props.textoSeguir}</Button>
+          <Button color="secondary" onClick={toggle}>Salir</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
 class App extends Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     // INSERTE AQUÍ EL ESTADO NECESARIO. AQUÍ SE GUARDARÁ TODA LA INFORMACIÓN
-     listaUsuarios: [
-      {nombre:"Paco", telefono:"12345", saldo:2},
-      {nombre:"Juan Carlos", telefono:"123", saldo:3},
-      {nombre:"Juan Carlos", telefono:"12345557", saldo:0},
-      {nombre:"Juan Carlos", telefono:"12345527", saldo:0},
-     ],
-     opcion: 0,
-     mostrarAlta:false,
-     mostrarBaja:false,
-     mostrarActualizar:false,
-     mensaje:""
-   };
- }
-
- //borra por telefono, sólo si su saldo es 0
- borrar(x){
-   //me traigo la lista 
-   let lista = JSON.parse(JSON.stringify(this.state.listaUsuarios));
-  //recibo el objeto
-  //miro si su saldo es 0 para poder borrarlo
-  if(x.saldo==0){
-  let aux= lista.filter((user)=>user.telefono !==x.telefono)
-
-  //console.log(aux);
-  this.setState({
-    listaUsuarios:aux
-    ,mensaje:"Usuario borrado con éxito"})
-
-
-  }else{
-    this.setState({mensaje:"No se puede borar este usuario"})
+  constructor(props) {
+    super(props);
+    this.state = {
+      preguntas: JSON.parse(JSON.stringify(PREGUNTAS)),
+      aciertos: 0,
+      fallos: 0,
+      textoPregunta: "",
+      respuesta: 0,
+      finalizado: 0,
+    };
   }
+  componentDidMount() {
 
-
- }
-
- agregarUsuario(n,t,s){
-  //me traigo la lista 
-  let lista = JSON.parse(JSON.stringify(this.state.listaUsuarios));
-
-  console.log("Recido: " +n + ","+ t+","+s)
-  //telefono único
-  //busco si el teléfono está en la lista
-  let estaRepe=lista.filter((v)=>v.telefono==t).length;
-
-  console.log(estaRepe)
-  if(estaRepe==0){
-    //lo agregamos
-    lista.push({
-      nombre:n,
-      telefono:t,
-      saldo:s,
-
-    });
-    console.log(lista)
-    this.setState({listaUsuarios:lista,
-    mensaje:"Usuario agregado"})
-
-  }else{
-    //este repetido
-    this.setState({
-      mensaje:"Teléfono repetido,inserte otro"})
-
+    this.siguiente();
   }
-
- }
-
- //pasa pasar de int a entero parseInt
- sumarSaldo(t,saldo){
-  //me traigo la lista 
-  let lista = JSON.parse(JSON.stringify(this.state.listaUsuarios));
-  //busco por el telefono si existe un usuario igual
-  let existeUser=lista.filter((user)=>user.telefono==t);
-  
-  //como no se pueden repetir, solo habrá un telefono igual
-  if(existeUser.length ==0){
-    //no está
-    this.setState({
-      mensaje:"Teléfono NO encontrado, pruebe con otro"})
-
+  aleatorio(min, max) {
+    var horquilla = max - min;
+    return Math.floor(Math.random() * horquilla + min);
   }
-  //esta
-  if(existeUser.length ==1)
-  {
-   let saldoUsuario=lista.filter((user)=>user.telefono==t).map((v)=>v.saldo)
-  console.log("Saldo viejo: "+ saldoUsuario[0])
-    
-    let nuevoSaldo= parseInt(saldoUsuario[0]) + parseInt(saldo);
-    console.log("Nuevo saldo: "+ nuevoSaldo)
-    //cojo al usuario a borrar, primero lo copio
-    let nuevoUsuario=existeUser;
-    let nNombre=nuevoUsuario.map((v)=>v.nombre)[0];
-    let nTel=nuevoUsuario.map((v)=>v.telefono)[0];
-     
-
-    //borro usuario existente
-    lista=lista.filter((user)=>user.telefono !==nTel);
-
-    //lo subo a la lista
-    lista.push( {nombre:nNombre,
-      telefono:nTel,
-      saldo:nuevoSaldo,}
-);
-      //NOTA JC*
-      //Borro e inserto de nuevo, un usuario igual con el saldo actualizado.
-    //lo encuentra
-    this.setState({listaUsuarios:lista,
-      mensaje:"Actualizado saldo"})
-
+  acierto() {
+    let aux = this.state.aciertos;
+    aux++;
+    this.setState({ aciertos: aux });
+    this.siguiente();
   }
-
- }
- restarSaldo(t,saldo){
-  //me traigo la lista 
-  let lista = JSON.parse(JSON.stringify(this.state.listaUsuarios));
-  //busco por el telefono si existe un usuario igual
-  let existeUser=lista.filter((user)=>user.telefono==t);
-  
-  //como no se pueden repetir, solo habrá un telefono igual
-  if(existeUser.length ==0){
-    //no está
-    this.setState({
-      mensaje:"Teléfono NO encontrado, pruebe con otro"})
-
+  fallo() {
+    let aux = this.state.fallos;
+    aux++;
+    this.setState({ fallos: aux });
+    this.siguiente();
   }
-  //esta
-  if(existeUser.length ==1)
-  {
-   let saldoUsuario=lista.filter((user)=>user.telefono==t).map((v)=>v.saldo)
-  console.log("Saldo viejo: "+ saldoUsuario[0])
-    
-    let nuevoSaldo= parseInt(saldoUsuario[0]) - parseInt(saldo);
-    console.log("Nuevo saldo: "+ nuevoSaldo)
-
-    if(nuevoSaldo <=0){
-      nuevoSaldo=0;
+  siguiente() {
+    console.log(this.state)
+    if (this.state.preguntas.length === 0) {
+      this.setState({ finalizado: 1 });
+      console.log("finalizando")
+      return;
     }
-    //cojo al usuario a borrar, primero lo copio
-    let nuevoUsuario=existeUser;
-    let nNombre=nuevoUsuario.map((v)=>v.nombre)[0];
-    let nTel=nuevoUsuario.map((v)=>v.telefono)[0];
-     
-
-    //borro usuario existente
-    lista=lista.filter((user)=>user.telefono !==nTel);
-
-    //lo subo a la lista
-    lista.push( {nombre:nNombre,
-      telefono:nTel,
-      saldo:nuevoSaldo,}
-);
-      //NOTA JC*
-      //Borro e inserto de nuevo, un usuario igual con el saldo actualizado.
-    //lo encuentra
-    this.setState({listaUsuarios:lista,
-      mensaje:"Actualizado saldo"})
-
+    let n = this.aleatorio(0, this.state.preguntas.length - 1);
+    let p = this.state.preguntas[n];
+    console.log(p);
+    let aux = this.state.preguntas.filter((e, inx) => inx !== n);
+    this.setState({
+      preguntas: aux, quest: p, textoPregunta: p.pregunta, respuesta: p.
+        respuesta
+    });
   }
+  seguir() {
 
- }
-
-
- mostrar(){
-console.log("deberia mostrar alta")
-this.setState({mostrarAlta:true,
-  mostrarActualizar:false,
-mostrarBaja:false})
- }
-
- mostrar2(){
-  console.log("deberia mostrar actualizar")
-  this.setState({mostrarAlta:false,
-    mostrarActualizar:true,
-  mostrarBaja:false})
-   }
-
-   mostrar3(){
-    console.log("deberia mostrar bajar")
-    this.setState({mostrarAlta:false,
-      mostrarActualizar:false,
-    mostrarBaja:true})
-     }
-
-
- render() {
-  let obj=<></>;
-  if(this.state.mostrarAlta){
-      obj=<>
-      <Altas alta={(n,t,s)=>this.agregarUsuario(n,t,s)}/>
-      </>
+    this.setState(
+      {
+        preguntas: JSON.parse(JSON.stringify(PREGUNTAS)), aciertos: 0, fallos: 0, finalizado: 0
+      });
   }
-  if(this.state.mostrarActualizar){
-    obj=<><Saldo actualizar={(tel,saldo)=>this.sumarSaldo(tel,saldo)}/></>
+  getTexto() {
+    let total = this.state.fallos + this.state.aciertos;
+    let porcentaje = this.state.aciertos * 100 / total;
+    if (porcentaje < 50) return "En principio no tienes nada de que preocuparte";
+    if (porcentaje < 70) return "Empiezas a tener signos de dependencia del móvil.Puedes utilizar técnicas como apagar el móvil cuando no lo necesitas, cuando duermes, etc.";
+    if (porcentaje < 80) return "Tienes una gran dependencia del móvil. Deberías seguir un plan de «desintoxicación» del móvil como por ejemplo dejar el móvil en casa cuando vas a comprar, apagarlo durante la noche, apagarlo durante horas de clase o trabajo, etc.";
+    return "Tus síntomas de dependencia son muy preocupantes. Además de todas las técnicas de los apartados anteriores deberías plantearte un plan de desintoxicación del móvil que consista en estar una o dos semanas sin utilizarlo.Si ves que no puedes hacerlo por ti mismo, pide ayuda a un profesional.";
   }
-  if(this.state.mostrarBaja){
-    obj=<><Saldo actualizar={(tel,saldo)=>this.restarSaldo(tel,saldo)}/> </>
+  render() {
+    let lista = [];
+    if (this.state.finalizado) {
+      let txt = this.getTexto();
+      lista.push(<VentanaModal textoSeguir="Hacerlo de nuevo"
+        seguir={() => this.seguir()} titulo="Fin del test" texto={txt} />);
+    } else {
+      if (this.state.respuesta === 1) {
+        lista.push(<Preguntas cabecera="Test Nomofobia"
+          si={() => this.acierto()} no={() => this.fallo()}
+          texto={this.state.textoPregunta} respuesta={this.state.respuesta} />);
+      } else {
+        lista.push(<Preguntas cabecera="Test Nomofobia"
+          si={() => this.fallo()} no={() => this.acierto()}
+          texto={this.state.textoPregunta} respuesta={this.state.respuesta} />);
+      }
 
+    }
+    console.log(this.state.finalizado)
+    return (
+      <div className="App">
+        <header className="App-header">
+          {lista}
+        </header>
+      </div>
+    );
   }
-
-
-
-
-   return (
-     <div className="App">
-       <h1>GESTION USUARIOS</h1>
-
-       <Mostrar datos={this.state.listaUsuarios} borrar={(t) => this.borrar(t)} />
-       <ButtonGroup>
-         {/* <Button color="info" datos={this.state.listaUsuarios}
-         alta={(n,t,s)=>this.agregarUsuario(n,t,s)}
-         onClick={()=>alta()}
-         > */}
-         <Button color="info" onClick={()=>this.mostrar()}>
-           Alta usuario
-         </Button>
-         <Button color="success" onClick={()=>this.mostrar2()}>
-           Sumar saldo
-         </Button>
-         <Button color="danger"  onClick={()=>this.mostrar3()}>
-           Gastar saldo
-         </Button>
-       </ButtonGroup>
-
-       {obj}
-
-    
-         {/* me tengo que traer telefono y saldo */}
-       {/* <Saldo actualizar={(tel,saldo)=>this.sumarSaldo(tel,saldo)}/> */}
-
-       {/* <h2>Gastar Saldo</h2> */}
-         {/* me tengo que traer telefono y saldo */}
-        {/* <Saldo actualizar={(tel,saldo)=>this.restarSaldo(tel,saldo)}/>  */}
-         
-       <h2>{this.state.mensaje}</h2>
-
-
-     </div>
-   );
- }
 }
 export default App;
